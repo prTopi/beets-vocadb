7de9add52405457660d4fc160e6c1c6ac8cbf7bc
Konstantin <konstantinkreutz@gmail.com>
2024-08-18 12:13:28 +0200

    Let's see...

--- beetsplug/touhoudb.py
+++ beetsplug/touhoudb.py
@@ -2,7 +2,7 @@ from beetsplug.vocadb import VocaDBPlugin
 
 class TouhouDBPlugin(VocaDBPlugin):
     def __init__(self):
-        super(TouhouDBPlugin, self).__init__()
+        super().__init__()
         self.name = "TouhouDB"
         self.base_url = "https://touhoudb.com/"
         self.api_url = "https://touhoudb.com/api/"
--- beetsplug/utaitedb.py
+++ beetsplug/utaitedb.py
@@ -2,7 +2,7 @@ from beetsplug.vocadb import VocaDBPlugin
 
 class UtaiteDBPlugin(VocaDBPlugin):
     def __init__(self):
-        super(UtaiteDBPlugin, self).__init__()
+        super().__init__()
         self.name = "UtaiteDB"
         self.base_url = "https://utaiteadb.net/"
         self.api_url = "https://utaitedb.net/api/"
