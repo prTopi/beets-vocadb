54beaf90450cb46acc74c300f89c610c57d95b4b
Konstantin <konstantinkreutz@gmail.com>
2024-12-15 21:42:45 +0100

    Explicit typing in tests

--- tests/test_utaitedb.py
+++ tests/test_utaitedb.py
@@ -1,6 +1,7 @@
 from beetsplug.utaitedb import UtaiteDBPlugin
 from tests.test_vocadb import TestVocaDBPlugin
 
+
 class TestUtaiteDBPlugin(TestVocaDBPlugin):
-    def setUp(self):
+    def setUp(self) -> None:
         self.plugin = UtaiteDBPlugin()
