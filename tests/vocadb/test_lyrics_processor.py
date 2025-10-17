8126baaa53efb8b591c1ac8246fc61219d724a7c
Konstantin <78656278+amogus07@users.noreply.github.com>
2025-10-17 18:32:04 +0200

    refactor tests, use pytest instead of unittest

--- tests/vocadb/test_lyrics_processor.py
+++ tests/vocadb/test_lyrics_processor.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 import sys
-from unittest import TestCase
+from typing import NamedTuple
 
 import msgspec
+import pytest
 
 from beetsplug.vocadb.lyrics_processor import LyricsProcessor
 from beetsplug.vocadb.vocadb_api_client import (
@@ -15,162 +18,177 @@ else:
     pass
 
 
-class TestLyricsProcessor(TestCase):
-    lyrics_processors: dict[ContentLanguagePreference, LyricsProcessor] = {
-        language_preference: LyricsProcessor(
-            language_preference=language_preference
-        )
-        for language_preference in ContentLanguagePreference
+class LyricsTestCase(NamedTuple):
+    lyrics: str
+    expected_language_code: str | None
+    expected_script: str | None
+    language_preference_expected_lyrics_mapping: list[
+        tuple[ContentLanguagePreference, str | None]
+    ]
+
+
+class TestLyricsProcessor:
+    @pytest.mark.parametrize(
+        argnames="test_case",
+        argvalues=[
+            LyricsTestCase(
+                lyrics="""[
+    {
+        "cultureCodes": ["ja"],
+        "id": 0,
+        "translationType": "Original",
+        "value": "lyrics1"
+    },
+    {
+        "cultureCodes": ["en"],
+        "id": 0,
+        "source": "FooBar",
+        "translationType": "Translation",
+        "url": "https://foo.bar",
+        "value": "lyrics2"
+    },
+    {
+        "cultureCodes": [""],
+        "id": 0,
+        "translationType": "Romanized",
+        "value": "lyrics3"
     }
-
-    def test_get_lyrics(self) -> None:
-        lyrics: list[LyricsForSongContract] = msgspec.json.decode(
-            """[
-                {
-                    "cultureCodes": ["ja"],
-                    "id": 0,
-                    "translationType": "Original",
-                    "value": "lyrics1"
-                },
-                {
-                    "cultureCodes": ["en"],
-                    "id": 0,
-                    "source": "FooBar",
-                    "translationType": "Translation",
-                    "url": "https://foo.bar",
-                    "value": "lyrics2"
-                },
-                {
-                    "cultureCodes": [""],
-                    "id": 0,
-                    "translationType": "Romanized",
-                    "value": "lyrics3"
-                }
-            ]""",
-            type=list[LyricsForSongContract],
+]""",
+                expected_language_code="Jpan",
+                expected_script="jpn",
+                language_preference_expected_lyrics_mapping=[
+                    (ContentLanguagePreference.JAPANESE, "lyrics1"),
+                    (ContentLanguagePreference.ENGLISH, "lyrics2"),
+                    (ContentLanguagePreference.ROMAJI, "lyrics3"),
+                    (ContentLanguagePreference.DEFAULT, "lyrics1"),
+                ],
+            ),
+            LyricsTestCase(
+                lyrics="""[
+        {
+            "cultureCodes": ["ja"],
+            "id": 0,
+            "translationType": "Translation",
+            "value": "lyrics1"
+        },
+        {
+            "cultureCodes": ["en"],
+            "id": 0,
+            "translationType": "Original",
+            "value": "lyrics2"
+        }
+    ]""",
+                expected_language_code="Latn",
+                expected_script="eng",
+                language_preference_expected_lyrics_mapping=[
+                    (ContentLanguagePreference.JAPANESE, "lyrics1"),
+                    (ContentLanguagePreference.ENGLISH, "lyrics2"),
+                ],
+            ),
+            LyricsTestCase(
+                lyrics="""[
+    {
+        "cultureCodes": ["ja"],
+        "id": 0,
+        "translationType": "Original",
+        "value": "lyrics1"
+    }
+]""",
+                expected_language_code="Jpan",
+                expected_script="jpn",
+                language_preference_expected_lyrics_mapping=[
+                    (ContentLanguagePreference.ENGLISH, "lyrics1")
+                ],
+            ),
+            LyricsTestCase(
+                lyrics="""[
+        {
+            "cultureCodes": ["ja"],
+            "id": 0,
+            "translationType": "Original",
+            "value": "lyrics1"
+        },
+        {
+            "cultureCodes": ["en"],
+            "id": 0,
+            "translationType": "Translation",
+            "value": "lyrics2"
+        },
+        {
+            "cultureCodes": [""],
+            "id": 0,
+            "translationType": "Romanized",
+            "value": "lyrics3"
+        }
+    ]""",
+                expected_language_code="Jpan",
+                expected_script="jpn",
+                language_preference_expected_lyrics_mapping=[
+                    (ContentLanguagePreference.JAPANESE, "lyrics1"),
+                    (ContentLanguagePreference.ENGLISH, "lyrics2"),
+                    (ContentLanguagePreference.ROMAJI, "lyrics3"),
+                    (ContentLanguagePreference.DEFAULT, "lyrics1"),
+                ],
+            ),
+            LyricsTestCase(
+                lyrics="""[
+        {
+            "cultureCodes": ["ja"],
+            "id": 0,
+            "translationType": "Translation",
+            "value": "lyrics1"
+        },
+        {
+            "cultureCodes": ["en"],
+            "id": 0,
+            "translationType": "Original",
+            "value": "lyrics2"
+        }
+    ]""",
+                expected_language_code="Latn",
+                expected_script="eng",
+                language_preference_expected_lyrics_mapping=[
+                    (ContentLanguagePreference.JAPANESE, "lyrics1"),
+                    (ContentLanguagePreference.ENGLISH, "lyrics2"),
+                    (ContentLanguagePreference.DEFAULT, "lyrics2"),
+                ],
+            ),
+            LyricsTestCase(
+                lyrics="""[
+        {
+            "cultureCodes": ["ja"],
+            "id": 0,
+            "translationType": "Original",
+            "value": "lyrics1"
+        }
+    ]""",
+                expected_language_code="Jpan",
+                expected_script="jpn",
+                language_preference_expected_lyrics_mapping=[
+                    (ContentLanguagePreference.DEFAULT, "lyrics1")
+                ],
+            ),
+        ],
+    )
+    def test_get_lyrics(
+        self,
+        test_case: LyricsTestCase,
+    ) -> None:
+        decoded_lyrics: list[LyricsForSongContract] = msgspec.json.decode(
+            test_case.lyrics, type=list[LyricsForSongContract]
         )
-        assert self.lyrics_processors[
-            ContentLanguagePreference.JAPANESE
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics1")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.ENGLISH
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics2")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.ROMAJI
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics3")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.DEFAULT
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics1")
-        lyrics = msgspec.json.decode(
-            """[
-                {
-                    "cultureCodes": ["ja"],
-                    "id": 0,
-                    "translationType": "Translation",
-                    "value": "lyrics1"
-                },
-                {
-                    "cultureCodes": ["en"],
-                    "id": 0,
-                    "translationType": "Original",
-                    "value": "lyrics2"
-                }
-            ]""",
-            type=list[LyricsForSongContract],
-        )
-        assert self.lyrics_processors[
-            ContentLanguagePreference.JAPANESE
-        ].get_lyrics(lyrics) == ("Latn", "eng", "lyrics1")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.ENGLISH
-        ].get_lyrics(lyrics) == ("Latn", "eng", "lyrics2")
-        lyrics = msgspec.json.decode(
-            """[
-                {
-                    "cultureCodes": ["ja"],
-                    "id": 0,
-                    "translationType": "Original",
-                    "value": "lyrics1"
-                }
-            ]""",
-            type=list[LyricsForSongContract],
-        )
-        assert self.lyrics_processors[
-            ContentLanguagePreference.ENGLISH
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics1")
-
-        lyrics = msgspec.json.decode(
-            """[
-                {
-                    "cultureCodes": ["ja"],
-                    "id": 0,
-                    "translationType": "Original",
-                    "value": "lyrics1"
-                },
-                {
-                    "cultureCodes": ["en"],
-                    "id": 0,
-                    "translationType": "Translation",
-                    "value": "lyrics2"
-                },
-                {
-                    "cultureCodes": [""],
-                    "id": 0,
-                    "translationType": "Romanized",
-                    "value": "lyrics3"
-                }
-            ]""",
-            type=list[LyricsForSongContract],
-        )
-        assert self.lyrics_processors[
-            ContentLanguagePreference.JAPANESE
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics1")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.ENGLISH
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics2")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.ROMAJI
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics3")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.DEFAULT
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics1")
-        lyrics = msgspec.json.decode(
-            """[
-                {
-                    "cultureCodes": ["ja"],
-                    "id": 0,
-                    "translationType": "Translation",
-                    "value": "lyrics1"
-                },
-                {
-                    "cultureCodes": ["en"],
-                    "id": 0,
-                    "translationType": "Original",
-                    "value": "lyrics2"
-                }
-            ]""",
-            type=list[LyricsForSongContract],
-        )
-        assert self.lyrics_processors[
-            ContentLanguagePreference.JAPANESE
-        ].get_lyrics(lyrics) == ("Latn", "eng", "lyrics1")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.ENGLISH
-        ].get_lyrics(lyrics) == ("Latn", "eng", "lyrics2")
-        assert self.lyrics_processors[
-            ContentLanguagePreference.DEFAULT
-        ].get_lyrics(lyrics) == ("Latn", "eng", "lyrics2")
-        lyrics = msgspec.json.decode(
-            """[
-                {
-                    "cultureCodes": ["ja"],
-                    "id": 0,
-                    "translationType": "Original",
-                    "value": "lyrics1"
-                }
-            ]""",
-            type=list[LyricsForSongContract],
-        )
-        assert self.lyrics_processors[
-            ContentLanguagePreference.DEFAULT
-        ].get_lyrics(lyrics) == ("Jpan", "jpn", "lyrics1")
+        expected_language_code: str | None = test_case.expected_language_code
+        expected_script: str | None = test_case.expected_script
+        language_preference: ContentLanguagePreference
+        expected_lyrics: str | None
+        for (
+            language_preference,
+            expected_lyrics,
+        ) in test_case.language_preference_expected_lyrics_mapping:
+            assert LyricsProcessor(
+                language_preference=language_preference
+            ).get_lyrics(decoded_lyrics) == (
+                expected_language_code,
+                expected_script,
+                expected_lyrics,
+            )
--- tests/vocadb/test_utils.py
+++ tests/vocadb/test_utils.py
@@ -1,70 +1,79 @@
-from unittest import TestCase
+from __future__ import annotations
 
 import msgspec
+import pytest
 
 from beetsplug.vocadb.utils import get_genres
 from beetsplug.vocadb.vocadb_api_client import TagUsageForApiContract
 
 
-class TestUtils(TestCase):
-    def test_get_genres(self) -> None:
-        tags: list[TagUsageForApiContract] = []
-        assert get_genres(tags) is None
-        tags = msgspec.json.decode(
+@pytest.mark.parametrize(
+    argnames="remote_tags, expected",
+    argvalues=[
+        ("[]", None),
+        (
             """[
-                {
-                    "count": 0,
-                    "tag": {
-                        "categoryName": "Genres",
-                        "id": 0,
-                        "name": "genre1"
-                    }
-                }
-            ]""",
-            type=list[TagUsageForApiContract],
-        )
-        assert get_genres(tags) == "Genre1"
-        tags = msgspec.json.decode(
+    {
+        "count": 0,
+        "tag": {
+            "categoryName": "Genres",
+            "id": 0,
+            "name": "genre1"
+        }
+    }
+]""",
+            "Genre1",
+        ),
+        (
             """[
-                {
-                    "count": 2,
-                    "tag": {
-                        "categoryName": "Genres",
-                        "id": 0,
-                        "name": "genre1"
-                    }
-                },
-                {
-                    "count": 1,
-                    "tag": {
-                        "categoryName": "Genres",
-                        "id": 0,
-                        "name": "genre2"
-                    }
-                }
-            ]""",
-            type=list[TagUsageForApiContract],
-        )
-        assert get_genres(tags) == "Genre1; Genre2"
-        tags = msgspec.json.decode(
+    {
+        "count": 2,
+        "tag": {
+            "categoryName": "Genres",
+            "id": 0,
+            "name": "genre1"
+        }
+    },
+    {
+        "count": 1,
+        "tag": {
+            "categoryName": "Genres",
+            "id": 0,
+            "name": "genre2"
+        }
+    }
+]""",
+            "Genre1; Genre2",
+        ),
+        (
             """[
-                {
-                    "count": 2,
-                    "tag": {
-                        "categoryName": "Vocalists",
-                        "id": 0,
-                        "name": "genre1"
-                    }
-                },
-                {
-                    "count": 1,
-                    "tag": {
-                        "categoryName": "Genres",
-                        "id": 0,
-                        "name": "genre2"
-                    }
-                }
-            ]""",
-            type=list[TagUsageForApiContract],
+    {
+        "count": 2,
+        "tag": {
+            "categoryName": "Vocalists",
+            "id": 0,
+            "name": "genre1"
+        }
+    },
+    {
+        "count": 1,
+        "tag": {
+            "categoryName": "Genres",
+            "id": 0,
+            "name": "genre2"
+        }
+    }
+]""",
+            "Genre2",
+        ),
+    ],
+)
+def test_get_genres(remote_tags: str, expected: str | None) -> None:
+    assert (
+        get_genres(
+            remote_tags=msgspec.json.decode(
+                remote_tags, type=list[TagUsageForApiContract]
+            )
         )
-        assert get_genres(tags) == "Genre2"
+        == expected
+    )
