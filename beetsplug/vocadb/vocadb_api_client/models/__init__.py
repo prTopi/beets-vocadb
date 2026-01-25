dca864a9b2194853ed363aab3de81f7b70061a98
Konstantin <78656278+amogus07@users.noreply.github.com>
2026-01-25 16:31:57 +0100

    Update to Python 3.10

--- beetsplug/vocadb/vocadb_api_client/models/__init__.py
+++ beetsplug/vocadb/vocadb_api_client/models/__init__.py
@@ -16,10 +16,7 @@ if not sys.version_info < (3, 11):
 else:
     from backports.strenum import StrEnum
 
-if not sys.version_info < (3, 10):
-    from typing import TypeAlias  # pyright: ignore[reportUnreachable]
-else:
-    from typing_extensions import TypeAlias
+from typing import TypeAlias
 
 
 class PascalCaseStrEnum(StrEnum):
